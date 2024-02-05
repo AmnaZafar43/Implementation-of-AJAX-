9af6b5eb9498c1a5b2ec580ae216b758f6c18fce
Amna Zafar <Amnazafar052@gmail.com>
2024-02-05 17:19:22 +0500

    Implementation of AJAX
    Select the category of product and data is fetched from api without reloading the page.

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("AJAX");
+
+let fetchBtn = document.getElementById("fetchBtn");
+let backupBtn = document.getElementById("backupBtn");
+
+fetchBtn.addEventListener("click", fetchData);
+
+function fetchData(category) {
+  // create xhr() object
+  const xhr = new XMLHttpRequest();
+
+  // open the object
+  xhr.open(
+    "GET",
+    category === "Electronics"
+      ? "https://fakestoreapi.com/products/category/electronics"
+      : "https://fakestoreapi.com/products/category/jewelery",
+    true
+  ); //true means async
+
+  // what to do on progress
+  xhr.onprogress = function () {
+    console.log("On Progress");
+  };
+
+  // when everything(response) is ready
+  xhr.onload = function () {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      // handle the fetched data
+      document.getElementById("data-container").innerHTML =
+        "<p>" + xhr.responseText + "</p>";
+    } else {
+      // Error - handle the error
+      console.log("Error fetching data. Status:", xhr.status);
+    }
+  };
+  xhr.onerror = function () {
+    console.log("Network error occurred");
+  };
+  // send the request
+  xhr.send();
+}
